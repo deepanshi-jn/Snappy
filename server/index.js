342465565b9cca04fda45c4ef69b56ca5a7eba51
deepanshiiiiii <126682692+deepanshi-jn@users.noreply.github.com>
2025-03-09 23:48:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const port=process.env.PORT || 3001
 const databaseURL=process.env.DATABASE_URL
 
 app.use(cors({
-    origin:process.env.ORIGIN || "http://localhost:5173",
+    origin:process.env.ORIGIN,
     methods:["GET","POST","PUT","PATCH","DELETE"],
     credentials:true
 }))
